73c039fb0682adcf20d70377aaceb9d169edc7fa
Jai <jksmithnyc@gmail.com>
2021-09-22 14:09:31 -0400

    add firebase config, websockets via firebase, and qrcode scanning

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,77 @@
 import { QRCode } from 'react-qrcode-logo';
 import './App.css';
+import { getFirestore, collection, doc, setDoc, addDoc, deleteDoc, DocumentReference, DocumentData, onSnapshot } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { parse } from 'query-string';
 
 const QR_LOGO_HEIGHT = 100;
 
-function App() {  
+function App() {
+  const db = getFirestore();
+
+  const [qrcodeDoc, setQrcodeDoc] = useState<DocumentReference<DocumentData> | null>(null);
+  const [scanned, setScanned] = useState<boolean>(false);
+
+  const { search } = useLocation();
+  const { id } = parse(search);
+
+  useEffect(() => {
+    if (!qrcodeDoc && !id) {
+      addDoc(collection(db, 'qrcodes'), {
+        scanned: false
+      }).then((ref) => {
+        setQrcodeDoc(ref);
+
+        // todo - unsubscribe
+        onSnapshot(doc(db, 'qrcodes', ref.id), (snapshotRef) => {
+          const data = snapshotRef.data();
+          const { scanned } = data || {};
+          if (scanned) {
+            setScanned(scanned);
+            deleteDoc(doc(db, 'qrcodes', ref.id));
+          }
+        });
+      });
+    }
+  }, [db, qrcodeDoc, id]);
+  
+  if (id) {
+    setDoc(doc(db, 'qrcodes', id as string), {
+      scanned: true
+    });
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {id}
+        </header>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        Scan me!
-        <QRCode
-          value="https://google.com"
-          qrStyle="dots"
-          eyeRadius={4}
-          bgColor="transparent"
-          fgColor="white"
-          logoImage="lone-pine.png"
-          logoWidth={QR_LOGO_HEIGHT*.5673}
-          logoHeight={QR_LOGO_HEIGHT}
-          logoOpacity={0.4}
-          quietZone={20}
-        />
+        {qrcodeDoc
+          ? (
+            <>
+              Scan me! ({scanned ? 'scanned!' : 'not scanned'})
+              <QRCode
+                value={`https://cs98-hackathing-1.web.app?id=${qrcodeDoc.id}`}
+                qrStyle="dots"
+                eyeRadius={4}
+                bgColor="transparent"
+                fgColor="white"
+                logoImage="lone-pine.png"
+                logoWidth={QR_LOGO_HEIGHT*.5673}
+                logoHeight={QR_LOGO_HEIGHT}
+                logoOpacity={0.2}
+                quietZone={20}
+              />
+            </>
+          ) : 'loading...'
+        }
       </header>
     </div>
   );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+import { BrowserRouter } from 'react-router-dom';
+
+initializeApp({
+  apiKey: "AIzaSyDE-HjMTCBLVjO-BpI5cQiBS9fNk48xF5k",
+  authDomain: "cs98-hackathing-1.firebaseapp.com",
+  projectId: "cs98-hackathing-1",
+  storageBucket: "cs98-hackathing-1.appspot.com",
+  messagingSenderId: "750752646678",
+  appId: "1:750752646678:web:73275e943c24019b931737"
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
